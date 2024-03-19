9817328cc9a1a6f8d501460b2e7f26ed9bcbb951
efgrthyjkuilo <147861361+1054009@users.noreply.github.com>
2024-03-18 23:27:38 -0400

    Add laser smg

---
+++ fun_guns/lua/weapons/fg_laser_smg.lua
@@ -0,0 +1,25 @@
+SWEP.Base = "fg_laser_pistol"
+
+local base_class = fg_base.SetupSWEP(SWEP, "Laser SMG")
+
+SWEP.ViewModel = "models/weapons/v_smg1.mdl"
+
+SWEP.Primary.Ammo = "SMG1"
+
+SWEP.Primary.ClipSize = 45
+
+SWEP.Primary.DefaultClip = 45
+
+SWEP.Primary.Automatic = true
+
+SWEP.Primary.Damage = 32
+
+SWEP.Primary.Spread = 0.2
+
+SWEP.PrimaryFireInterval = 0.1
+
+function SWEP:PostInitialize()
+	base_class.PostInitialize(self)
+
+	self:SetHoldType("smg")
+end
