5c30906c31c28a844f42821db84fa9baad3f2695
efgrthyjkuilo <147861361+1054009@users.noreply.github.com>
2024-03-18 20:09:28 -0400

    Laser pistol

---
+++ fun_guns/lua/weapons/fg_laser_pistol.lua
@@ -0,0 +1,27 @@
+SWEP.Base = "fg_base"
+
+local base_class = fg_base.SetupSWEP(SWEP, "Laser Pistol")
+
+function SWEP:PostInitialize()
+	self:SetHoldType("pistol")
+end
+
+function SWEP:PrimaryAttack()
+	if not self:CanPrimaryAttack() then return end
+
+	local tr = self:RunTrace()
+
+	if tr.Hit then
+		local effect_data = EffectData()
+
+		effect_data:SetStart(tr.StartPos)
+		effect_data:SetEntity(self)
+		effect_data:SetAttachment(1)
+		effect_data:SetOrigin(tr.HitPos)
+
+		util.Effect("laser", effect_data)
+	end
+
+	self:TakePrimaryAmmo(1)
+	self:SetNextPrimaryFire(self:GetNextPrimaryFireTime())
+end
