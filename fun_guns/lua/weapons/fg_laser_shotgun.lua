395f7933029a8614dd94a6f0930dd28475a1bd7c
Zach <147861361+1054009@users.noreply.github.com>
2024-03-19 13:06:17 -0400

    Add shotgun reloading

--- fun_guns/lua/weapons/fg_laser_shotgun.lua
+++ fun_guns/lua/weapons/fg_laser_shotgun.lua
@@ -24,8 +24,99 @@ SWEP.Primary.HorizontalViewPunch = 0
 
 SWEP.PrimaryFireInterval = 1
 
+SWEP.ReloadState = fg_base.RELOAD_STATE_FINISHED
+
 function SWEP:PostInitialize()
 	base_class.PostInitialize(self)
 
 	self:SetHoldType("shotgun")
+
+	self.ReloadState = fg_base.RELOAD_STATE_FINISHED
+end
+
+function SWEP:GetReloadState()
+	return self.ReloadState
+end
+
+function SWEP:SetReloadState(state)
+	self.ReloadState = tonumber(state) or fg_base.RELOAD_STATE_FINISHED
+end
+
+function SWEP:StartReload()
+	self:SetReloadState(fg_base.RELOAD_STATE_START)
+
+	self:SetBodygroup(1, 0) -- Make the shell visible
+
+	self:SendWeaponAnim(ACT_SHOTGUN_RELOAD_START)
+	self:SetNextPrimaryFire(CurTime() + self:SequenceDuration())
+end
+
+function SWEP:Reload()
+	if not self:HasAmmo() then return end -- All gone!
+	if CurTime() < self:GetNextPrimaryFire() then return end
+
+	if self:GetReserveAmmo(self:GetPrimaryAmmoType()) <= 0 then -- Out of reserve ammo
+		return
+	end
+
+	if self:GetReloadState() == fg_base.RELOAD_STATE_FINISHED then
+		self:StartReload()
+	end
+end
+
+function SWEP:DoReload()
+	if self:GetReloadState() ~= fg_base.RELOAD_STATE_ONGOING then
+		return
+	end
+
+	local owner = self:GetOwner()
+	if not IsValid(owner) then
+		return self:FinishReload()
+	end
+
+	if self:GetReserveAmmo(self:GetPrimaryAmmoType()) <= 0 or self:Clip1() >= self:GetMaxClip1() then
+		return self:FinishReload()
+	end
+
+	-- https://github.com/Facepunch/garrysmod-issues/issues/3269
+	if CurTime() < self:GetNextPrimaryFire() then return end
+
+	self:TakeAmmo(-1, self:GetPrimaryAmmoType())
+
+	self:SendWeaponAnim(ACT_VM_RELOAD)
+	self:SetNextPrimaryFire(CurTime() + self:SequenceDuration())
+
+	self:NextThink(self:GetNextPrimaryFire())
+
+	if CLIENT then
+		self:SetNextClientThink(self:GetNextPrimaryFire())
+	end
+
+	return true
+end
+
+function SWEP:FinishReload()
+	if self:GetReloadState() ~= fg_base.RELOAD_STATE_ONGOING then
+		return
+	end
+
+	self:SetReloadState(fg_base.RELOAD_STATE_FINISHED)
+
+	self:SendWeaponAnim(ACT_SHOTGUN_RELOAD_FINISH)
+	self:SetNextPrimaryFire(CurTime() + self:SequenceDuration())
+end
+
+function SWEP:OnThink()
+	local reload_state = self:GetReloadState()
+
+	local is_started = reload_state == fg_base.RELOAD_STATE_START
+	local is_ongoing = reload_state == fg_base.RELOAD_STATE_ONGOING
+
+	if is_started or is_ongoing then
+		if is_started then
+			self:SetReloadState(fg_base.RELOAD_STATE_ONGOING)
+		end
+
+		return self:DoReload()
+	end
 end
